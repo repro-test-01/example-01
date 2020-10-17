cd586ac2cae33d55dbe3d73151cc6d164b8a1a0f
repro-test-01 <73007308+repro-test-01@users.noreply.github.com>
2020-10-16 19:32:47 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_s3_bucket" "example01" {
+resource "aws_s3_bucket" "example-01" {
   bucket = "example-01"
   versioning {
     enabled    = true
